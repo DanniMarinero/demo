a470afe09a0dfd3554631962d809acd83ff161f5
DanniMarinero <160778103+DanniMarinero@users.noreply.github.com>
2024-02-21 12:50:18 -0600

    Add files via upload

---
+++ src/components/Product/LinkSvgIcon2.tsx
@@ -0,0 +1,36 @@
+import { memo, SVGProps } from 'react';
+
+const LinkSvgIcon2 = (props: SVGProps<SVGSVGElement>) => (
+  <svg preserveAspectRatio='none' viewBox='0 0 27 28' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path
+      d='M19.4976 19.6875H7.89912L5.06553 4.10781C5.03237 3.92124 4.93505 3.75212 4.79039 3.62971C4.64573 3.50731 4.46283 3.43931 4.27334 3.4375H2.43506'
+      stroke='black'
+      strokeWidth={1.625}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M8.93506 23.75C10.0569 23.75 10.9663 22.8406 10.9663 21.7188C10.9663 20.5969 10.0569 19.6875 8.93506 19.6875C7.81323 19.6875 6.90381 20.5969 6.90381 21.7188C6.90381 22.8406 7.81323 23.75 8.93506 23.75Z'
+      stroke='black'
+      strokeWidth={1.625}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M19.4976 23.75C20.6194 23.75 21.5288 22.8406 21.5288 21.7188C21.5288 20.5969 20.6194 19.6875 19.4976 19.6875C18.3757 19.6875 17.4663 20.5969 17.4663 21.7188C17.4663 22.8406 18.3757 23.75 19.4976 23.75Z'
+      stroke='black'
+      strokeWidth={1.625}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M7.15771 15.625H19.914C20.2938 15.6262 20.6618 15.4934 20.9535 15.2501C21.2451 15.0067 21.4416 14.6684 21.5085 14.2945L22.7476 7.5H5.68506'
+      stroke='black'
+      strokeWidth={1.625}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+const Memo = memo(LinkSvgIcon2);
+export { Memo as LinkSvgIcon2 };
---
+++ src/components/Product/Product.tsx
@@ -0,0 +1,213 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import { Group247Icon } from './Group247Icon.js';
+import { LinkSvgIcon2 } from './LinkSvgIcon2.js';
+import { LinkSvgIcon3 } from './LinkSvgIcon3.js';
+import { LinkSvgIcon } from './LinkSvgIcon.js';
+import { ListItemLinkSvgIcon } from './ListItemLinkSvgIcon.js';
+import { NoimagePngIcon } from './NoimagePngIcon.js';
+import classes from './Product.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 719:810 */
+export const Product: FC<Props> = memo(function Product(props = {}) {
+  return (
+    <div className={`${resets.storybrainResets} ${classes.root}`}>
+      <div className={classes.rectangle3}></div>
+      <div className={classes.rectangle5}></div>
+      <div className={classes.startingPrice}>Starting price</div>
+      <div className={classes._1Coins}>0.01 Coins</div>
+      <div className={classes.rectangle4}></div>
+      <div className={classes.sp880AirpodsPro2ndGen1}></div>
+      <div className={classes.airPodsPro}>AirPods Pro</div>
+      <div className={classes.startingSoon}>Starting Soon</div>
+      <div className={classes._12h34m18s}> 12h : 34m : 18s</div>
+      <div className={classes.rectangle32}></div>
+      <div className={classes.rectangle52}></div>
+      <div className={classes.startingPrice2}>Starting price</div>
+      <div className={classes._1Coins2}>0.01 Coins</div>
+      <div className={classes.rectangle42}></div>
+      <div className={classes.sp880AirpodsPro2ndGen12}></div>
+      <div className={classes.airPodsPro2}>AirPods Pro</div>
+      <div className={classes.startingSoon2}>Starting Soon</div>
+      <div className={classes._12h34m18s2}> 12h : 34m : 18s</div>
+      <div className={classes.rectangle33}></div>
+      <div className={classes.rectangle53}></div>
+      <div className={classes.startingPrice3}>Starting price</div>
+      <div className={classes._1Coins3}>0.01 Coins</div>
+      <div className={classes.rectangle43}></div>
+      <div className={classes.sp880AirpodsPro2ndGen13}></div>
+      <div className={classes.airPodsPro3}>AirPods Pro</div>
+      <div className={classes.startingSoon3}>Starting Soon</div>
+      <div className={classes._12h34m18s3}> 12h : 34m : 18s</div>
+      <div className={classes.form}>
+        <div className={classes.divWhiteBg}>
+          <div className={classes.divColLg6}>
+            <div className={classes.heading4PlayStation5SlimDigita}>AirPods Pro</div>
+            <div className={classes.heading4PlayStation5SlimDigita2}>$200 USD</div>
+            <div className={classes.rectangle7}></div>
+            <div className={classes.buyNow}>Buy now</div>
+            <div className={classes.male1Png}></div>
+            <div className={classes.male2Png}></div>
+            <div className={classes.male3Png}></div>
+            <div className={classes.male4Png}></div>
+            <div className={classes.male5Png}></div>
+            <div className={classes.male6Png}></div>
+            <div className={classes.male7Png}></div>
+            <div className={classes.feb2439e464942f6Bc08D4d3f5bcb1}>
+              <div className={classes.sp880AirpodsPro2ndGen14}></div>
+            </div>
+          </div>
+          <div className={classes.divDvWinDiv}>
+            <div className={classes.heading4Label20759}>207.59 USD</div>
+            <div className={classes.heading4CurrentBid}>Current Bid:</div>
+            <div className={classes.separator}></div>
+            <div className={classes.noimagePng}>
+              <NoimagePngIcon className={classes.icon} />
+            </div>
+            <div className={classes.heading4CurrentHighestBidder}>Current Highest Bidder</div>
+            <div className={classes.heading4ChrisJohnny}>Chris Johnny</div>
+            <div className={classes.heading4SmallMemberSince}>Member since: 03 22, 2023</div>
+            <div className={classes.heading4SmallLabelPennyAuction}>Penny auction is number one site</div>
+            <div className={classes.separator2}></div>
+            <div className={classes.bid}>Bid</div>
+            <div className={classes.user}>User</div>
+            <div className={classes.date}>Date</div>
+            <div className={classes.divDvactiveBidderList}>
+              <div className={classes._20759USD}>207.59USD</div>
+              <div className={classes.chrisJohnny}>Chris Johnny</div>
+              <div className={classes._2152024}>2/15/2024</div>
+              <div className={classes._20758USD}>207.58USD</div>
+              <div className={classes.enriqueFreddie}>Enrique Freddie</div>
+              <div className={classes._21520242}>2/15/2024</div>
+              <div className={classes._20757USD}>207.57USD</div>
+              <div className={classes.elizabethBetty}>Elizabeth Betty</div>
+              <div className={classes._21520243}>2/15/2024</div>
+              <div className={classes._20756USD}>207.56USD</div>
+              <div className={classes.andyKirk}>Andy Kirk</div>
+              <div className={classes._21520244}>2/15/2024</div>
+              <div className={classes._20755USD}>207.55USD</div>
+              <div className={classes.carlosRussell}>Carlos Russell</div>
+              <div className={classes._21520245}>2/15/2024</div>
+              <div className={classes._20754USD}>207.54USD</div>
+              <div className={classes.maryPatricia}>Mary Patricia</div>
+              <div className={classes._21520246}>2/15/2024</div>
+              <div className={classes._20753USD}>207.53USD</div>
+              <div className={classes.kellyA}>kelly a</div>
+              <div className={classes._21520247}>2/15/2024</div>
+              <div className={classes._20752USD}>207.52USD</div>
+              <div className={classes.kimberlyDeborah}>Kimberly Deborah</div>
+              <div className={classes._21520248}>2/15/2024</div>
+              <div className={classes._20751USD}>207.51USD</div>
+              <div className={classes.chrisJohnny2}>Chris Johnny</div>
+              <div className={classes._21520249}>2/15/2024</div>
+              <div className={classes._2075USD}>207.5USD</div>
+              <div className={classes.enriqueFreddie2}>Enrique Freddie</div>
+              <div className={classes._215202410}>2/15/2024</div>
+            </div>
+            <div className={classes.withEachBidPriceWillGoUp}>With each bid price will go up</div>
+            <div className={classes.label05}>00 : 05</div>
+            <div className={classes.rectangle210}></div>
+            <div className={classes.bidNow}>Bid Now</div>
+          </div>
+        </div>
+        <div className={classes.divWhiteBg2}>
+          <div className={classes.heading4ProductDetails}>Product Details</div>
+          <div className={classes.loremIpsumDolorSitAmetConsecte}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit, duis odio leo cursus viverra enim. Lectus sem
+            elementum interdum eros risus turpis sociosqu ornare nisl, blandit pretium ultricies-
+          </div>
+        </div>
+      </div>
+      <div className={classes.form2}>
+        <div className={classes.divContainer}>
+          <div className={classes.link}>
+            <div className={classes.bids}>Bids </div>
+            <div className={classes.spanSpanavaialblebids}>
+              <div className={classes.unnamed}>0</div>
+            </div>
+          </div>
+          <div className={classes.link60cb47daB6f344318d4dC8a1dc}></div>
+          <div className={classes.input}>
+            <div className={classes.divPlaceholder}>
+              <div className={classes.searchByKeyword}>Search by Keyword</div>
+            </div>
+          </div>
+          <div className={classes.link2}>
+            <div className={classes.symbol}></div>
+          </div>
+          <div className={classes.linkSVG}>
+            <LinkSvgIcon className={classes.icon2} />
+          </div>
+          <div className={classes.linkSVG2}>
+            <LinkSvgIcon2 className={classes.icon3} />
+          </div>
+          <div className={classes.linkSVG3}>
+            <LinkSvgIcon3 className={classes.icon4} />
+          </div>
+        </div>
+      </div>
+      <div className={classes.form3}>
+        <div className={classes.divDeals}>
+          <div className={classes.listItemLinkSVG}>
+            <ListItemLinkSvgIcon className={classes.icon5} />
+          </div>
+          <div className={classes.listItemLinkHome}>Home</div>
+          <div className={classes.listItemLinkAboutUs}>About us</div>
+          <div className={classes.listItemLinkBuyBids}>Buy Bids</div>
+          <div className={classes.listItemLinkBlog}>Blog</div>
+          <div className={classes.listItemLinkContactUs}>Contact us</div>
+        </div>
+      </div>
+      <div className={classes.form4}>
+        <div className={classes.divContainer2}>
+          <div className={classes.listItemLinkPolicyInformation}>
+            <div className={classes.textBlock}>Policy &amp; Information</div>
+            <div className={classes.textBlock2}>
+              <p className={classes.labelWrapper}>
+                <span className={classes.label}>Terms and Conditions</span>
+              </p>
+            </div>
+          </div>
+          <div className={classes.listItemListLinkPaymentSecurit}>Payment Security Policy</div>
+          <div className={classes.listItemListLinkLegalDisclaime}>Legal Disclaimers</div>
+          <div className={classes.listItemLinkCategories}>
+            <div className={classes.textBlock3}>Categories</div>
+            <div className={classes.textBlock4}>
+              <p className={classes.labelWrapper2}>
+                <span className={classes.label2}>Tele Communication</span>
+              </p>
+            </div>
+          </div>
+          <div className={classes.listItemListLinkSportsAndEquip}>Sports and Equipment</div>
+          <div className={classes.listItemListLinkWomensFashion}>Womens Fashion</div>
+          <div className={classes.listItemListLinkComputerAndLap}>Computer and Laptop</div>
+          <div className={classes.listItemLinkInformation}>
+            <div className={classes.textBlock5}>Information</div>
+            <div className={classes.textBlock6}>
+              <p className={classes.labelWrapper3}>
+                <span className={classes.label3}>About</span>
+              </p>
+            </div>
+          </div>
+          <div className={classes.listItemListLinkBuyBids}>Buy Bids</div>
+          <div className={classes.listItemListLinkBlog}>Blog</div>
+          <div className={classes.listItemListLinkContact}>Contact</div>
+          <div className={classes.listItemFollowUs}>Follow Us</div>
+          <div className={classes.group247}>
+            <Group247Icon className={classes.icon6} />
+          </div>
+        </div>
+      </div>
+      <div className={classes.formFooter}>
+        <div className={classes._2024MrBidMe}>© 2024 - mrBid.me</div>
+        <div className={classes.smallDesignedAndDevelopedBy}>Designed and Developed by </div>
+        <div className={classes.smallLinkAMTechnology}>team marinero.mx</div>
+      </div>
+    </div>
+  );
+});
